cab6acd359dbec947d2c5ddb2cb8e41f13705250
Derek Clarkson <d4rkf1br3@gmail.com>
2016-12-05 11:46:31 +1100

    Adding arg for source directory name

--- xcode-highlight-tags.sh
+++ xcode-highlight-tags.sh
@@ -1,2 +1,2 @@
 echo "Finding all TODO: and FIXME: tags ..."
-grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift "${SRCROOT}" | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
+grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift "${SRCROOT}/$1" | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
