53d216715886954093e4c849275dcc7c41587356
Derek Clarkson <d4rkf1br3@gmail.com>
2016-12-05 11:33:55 +1100

    Still fixing

--- xcode-highlight-tags.sh
+++ xcode-highlight-tags.sh
@@ -1,2 +1,2 @@
 echo "Finding all TODO: and FIXME: tags ..."
-grep EioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift "${SRCROOT}" | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
+grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift "${SRCROOT}" | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
