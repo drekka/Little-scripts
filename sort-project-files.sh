d4066456c6d33e4658984e6a6b4d8460d3948fef
Derek Clarkson <d4rkf1br3@gmail.com>
2017-02-01 17:30:09 +1100

    Ignoring carthage on warnings
    Fixing bug in sorts.

--- sort-project-files.sh
+++ sort-project-files.sh
@@ -1,17 +1,25 @@
 
-# XCode writes the project file in an unsorted fashion on any change to the project. This can make mergese extremely difficult. Sorting the project file's contents removes a large amount of merge issues. 
+# XCode writes the project file in an unsorted fashion on any change to the project. This can make mergese extremely difficult. Sorting the project file's contents removes a large amount of merge issues.
 
 
 echo "Sorting project files which have been changed ..."
 
 script_name="External/SOCK/pysock/sock.py"
 
+function doSort {
+    while read file; do
+        echo "   Sorting $file"
+        python $script_name $file
+        git add $file
+    done
+}
+
 # Process:
 #    1. Find all changed and stages files
 #    2. Filter for project files
 #    3. Ignore the carthage check out directory
-#    4. Pipe through xargs and sort the files contents
+#    4. Pipe through the sort function.
 
-git diff --name-only --cached | grep project\.pbxproj$ | grep -v Carthage/Checkouts | xargs python $script_name
+git diff --name-only --cached | grep project\.pbxproj$ | grep -v Carthage/Checkouts | doSort
 
 echo "Done."
--- xcode-highlight-print-statements.sh
+++ xcode-highlight-print-statements.sh
@@ -1,2 +1,2 @@
 echo "Finding all print(...) ..."
-grep -REioHn "\sprint\(.*$" --include *.m --include *.h --include *.swift "${SRCROOT}/$1" | awk -F: '{printf "%s:%i: warning: print(...) statment\n",$1,$2}'
+grep -REioHn "\sprint\(.*$" --include *.m --include *.h --include *.swift "${SRCROOT}/$1" | grep -v Carthage/Checkouts | awk -F: '{printf "%s:%i: warning: print(...) statment\n",$1,$2}'
