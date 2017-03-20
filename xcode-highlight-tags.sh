16fe37e2c6b1bfdb71284ce4f85c385427648770
Derek Clarkson <derek.clarkson@auspost.com.au>
2017-03-21 10:52:43 +1100

    Adding pods to exclusions and removing args requirement.

--- xcode-highlight-tags.sh
+++ xcode-highlight-tags.sh
@@ -1,2 +1,2 @@
 echo "Finding all TODO: and FIXME: tags ..."
-grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift "${SRCROOT}/$1" | grep -v Carthage/Checkouts | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
+grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift . | grep -v \./Carthage/Checkouts | grep -v \./Pods | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
