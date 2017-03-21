9dc3d07f1fb0ca0d5bb5510c4cc8b08af9c8f729
Derek Clarkson <derek.clarkson@auspost.com.au>
2017-03-21 11:19:47 +1100

    Added —cache-builds to carthage updatge

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,2 +1,2 @@
 # Updates carthgate dependencies. Carthage options cab be passed as arguments.
-carthage update --no-use-binaries --platform iOS,tvOS $@
+carthage update --no-use-binaries --cache-builds --platform iOS,tvOS $@
