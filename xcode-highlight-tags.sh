888c8335c650884165be636e31e6e844ea17b36a
Derek Clarkson <derek.clarkson@auspost.com.au>
2017-04-20 15:10:55 +1000

    Adding a warning for WTF: tags.

--- xcode-highlight-tags.sh
+++ xcode-highlight-tags.sh
@@ -1,2 +1,2 @@
 echo "Finding all TODO: and FIXME: tags ..."
-grep -REioHn "(todo:|fixme:).*$" --include *.m --include *.h --include *.swift . | grep -v \./Carthage/Checkouts | grep -v \./Pods | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
+grep -REioHn "(todo:|fixme:|wtf:).*$" --include *.m --include *.h --include *.swift . | grep -v \./Carthage/Checkouts | grep -v \./Pods | awk -F: '{printf "%s:%i: warning: %s:%s\n",$1,$2,$3,$4}'
