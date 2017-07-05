23e2853202ae51b3e6f91c20ff262148bff2f873
Derek Clarkson <derek.clarkson@auspost.com.au>
2017-07-05 13:02:20 +1000

    Updating sripts

--- xcode-highlight-print-statements.sh
+++ xcode-highlight-print-statements.sh
@@ -1,2 +1,2 @@
 echo "Finding all print(...) ..."
-grep -REioHn "\sprint\(.*$" --include *.m --include *.h --include *.swift . | grep -v Carthage/Checkouts | grep -V \./Pods | awk -F: '{printf "%s:%i: warning: print(...) statment\n",$1,$2}'
+grep -REioHn "\sprint\(.*$" --include *.m --include *.h --include *.swift . | grep -v /Carthage | grep -v /Pods | awk -F: '{printf "%s:%i: warning: print(...) statment\n",$1,$2}'
